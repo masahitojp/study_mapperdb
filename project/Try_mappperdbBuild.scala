1cac9ea7927b1fc731081eeb27d803488a345fa0
masahitojp <randomstep@gmail.com>
2013-07-27 15:59:04 +0900

    add update method

--- project/Try_mappperdbBuild.scala
+++ project/Try_mappperdbBuild.scala
@@ -14,12 +14,12 @@ object Try_mappperdbBuild extends Build {
       libraryDependencies ++= Seq(
         // test
         "org.scalatest" %% "scalatest" % "1.9.1" % "test",
-	      "org.scalacheck" %% "scalacheck" % "1.10.1" % "test",
+        "org.scalacheck" %% "scalacheck" % "1.10.1" % "test",
         "com.h2database" % "h2" % "1.3.172",
         "commons-dbcp" % "commons-dbcp" % "1.4",
         "com.googlecode.mapperdao" % "mapperdao" % "1.0.0.rc23-2.10.1",
 
-  //log
+        //log
         "org.clapper" %% "grizzled-slf4j" % "1.0.1",
         "ch.qos.logback" % "logback-classic" % "1.0.13"
       )
